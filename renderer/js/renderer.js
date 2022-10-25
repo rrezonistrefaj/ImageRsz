dfe109d5cfeee8f6dfda775e3d5c4b471fa7314f
Rrezon Istrefaj <xoni-pr@hotmail.com>
2022-10-25 13:20:00 +0200

    version 1.0.0

---
+++ renderer/js/renderer.js
@@ -0,0 +1,89 @@
+const form = document.querySelector('#img-form');
+const img = document.querySelector('#img');
+const outputPath = document.querySelector('#output-path');
+const filename = document.querySelector('#filename');
+const heightInput = document.querySelector('#height');
+const widthInput = document.querySelector('#width');
+
+function loadImage(e) {
+  const file = e.target.files[0];
+
+  if (!isFileImage(file)) {
+    alertError('Please select an image');
+    return;
+  }
+
+
+  const image = new Image();
+  image.src = URL.createObjectURL(file);
+  image.onload = function () {
+    widthInput.value = this.width;
+    heightInput.value = this.height;
+  };
+
+  form.style.display = 'block';
+  filename.innerHTML = img.files[0].name;
+  outputPath.innerText = path.join(os.homedir(), 'imageresizer');
+}
+
+function isFileImage(file) {
+  const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+  return file && acceptedImageTypes.includes(file['type']);
+}
+
+function resizeImage(e) {
+  e.preventDefault();
+
+  if (!img.files[0]) {
+    alertError('Please upload an image');
+    return;
+  }
+
+  if (widthInput.value === '' || heightInput.value === '') {
+    alertError('Please enter a width and height');
+    return;
+  }
+
+  const imgPath = img.files[0].path;
+  const width = widthInput.value;
+  const height = heightInput.value;
+
+  ipcRenderer.send('image:resize', {
+    imgPath,
+    height,
+    width,
+  });
+}
+
+ipcRenderer.on('image:done', () =>
+  alertSuccess(`Image resized to ${heightInput.value} x ${widthInput.value}`)
+);
+
+function alertSuccess(message) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: 'green',
+      color: 'white',
+      textAlign: 'center',
+    },
+  });
+}
+
+function alertError(message) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: 'red',
+      color: 'white',
+      textAlign: 'center',
+    },
+  });
+}
+
+img.addEventListener('change', loadImage);
+form.addEventListener('submit', resizeImage);
